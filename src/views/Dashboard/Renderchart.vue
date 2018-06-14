b2835868a07f767dc53c954c62295a7931c308fb
VincentKempers <me@vncn.tk>
2018-06-14 07:11:03 +0200

    add chart to dashboard with fake data

--- src/views/Dashboard/Renderchart.vue
+++ src/views/Dashboard/Renderchart.vue
@@ -3,20 +3,38 @@ import { Line } from 'vue-chartjs';
 
 export default {
 	extends: Line,
+	data() {
+		return {
+			data: [0, 0, 0, 0, 1, 2, 0],
+		};
+	},
+	methods: {
+		manipulation() {
+			setTimeout(function() {
+				console.log(this);
+
+				this.data.pop();
+			}, 1000);
+		},
+		renderLineChart() {
+			this.renderChart(
+				{
+					labels: ['10.00', '10.30', '11.00', '11.30', '12.00', '12.30', '13.00'],
+					datasets: [
+						{
+							label: 'Data One',
+							backgroundColor: 'rgba(224, 90, 50, 1.000)',
+							data: this.data,
+						},
+					],
+				},
+				{ responsive: true, maintainAspectRatio: false }
+			);
+			this.manipulation();
+		},
+	},
 	mounted() {
-		this.renderChart(
-			{
-				labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-				datasets: [
-					{
-						label: 'Data One',
-						backgroundColor: '#f87979',
-						data: [40, 39, 10, 40, 39, 80, 40],
-					},
-				],
-			},
-			{ responsive: true, maintainAspectRatio: false }
-		);
+		this.renderLineChart();
 	},
 };
 </script>
