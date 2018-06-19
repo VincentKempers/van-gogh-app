18d59355181967530ebe65b39b3a4cab64a61324
VincentKempers <me@vncn.tk>
2018-06-19 09:42:50 +0200

    fixed the chart with the gradient and deleted the grid

--- src/views/Dashboard/Bigchart.vue
+++ src/views/Dashboard/Bigchart.vue
@@ -6,7 +6,9 @@ export default {
 	data() {
 		return {
 			data: [5, 10, 22, 17, 15, 20, 13],
+			secondData: [22, 17, 15, 20, 15, 11, 13],
 			gradient: null,
+			gradient2: null,
 		};
 	},
 	methods: {
@@ -30,19 +32,58 @@ export default {
 							backgroundColor: this.gradient,
 							data: this.data,
 						},
+						{
+							label: 'Data two',
+							borderColor: 'rgb(65, 184, 131)',
+							pointBackgroundColor: 'white',
+							borderWidth: 1,
+							pointBorderColor: 'white',
+							backgroundColor: this.gradient2,
+							data: this.secondData,
+						},
 					],
 				},
-				{ responsive: true, maintainAspectRatio: false }
+				{
+					scales: {
+						yAxes: [
+							{
+								gridLines: {
+									display: false,
+								},
+							},
+						],
+						xAxes: [
+							{
+								gridLines: {
+									display: false,
+								},
+							},
+						],
+					},
+					legend: {
+						display: true,
+					},
+					responsive: true,
+					maintainAspectRatio: false,
+				}
 			);
 			this.manipulation();
 		},
 	},
 	mounted() {
+		// gradient color 1
 		this.gradient = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450);
 
 		this.gradient.addColorStop(0, 'rgba(255, 0,0, 0.5)');
 		this.gradient.addColorStop(0.5, 'rgba(255, 0, 0, 0.25)');
 		this.gradient.addColorStop(1, 'rgba(255, 0, 0, 0)');
+
+		// gradient color 2
+		this.gradient2 = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450);
+
+		this.gradient2.addColorStop(0, 'rgb(65, 184, 131, 0.1)');
+		this.gradient2.addColorStop(0.5, 'rgb(65, 184, 131, 0.5)');
+		this.gradient2.addColorStop(1, 'rgb(65, 184, 131, 0.25)');
 		this.renderLineChart();
 	},
 };
